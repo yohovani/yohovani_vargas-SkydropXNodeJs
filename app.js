a7257683661c711cce637977850e1739f83043c9
yohovani <yohovanivargas@gmail.com>
2021-12-16 14:13:56 -0600

    Definición de endpoints

--- app.js
+++ app.js
@@ -26,9 +26,6 @@ app.use(function (req, res, next) {
 })
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,38 @@ var router = express.Router();
 
 const { Pool, Client } = require('pg')
 const pool = new Pool({
-  user: '',
+  user: 'cliente',
   host: 'localhost',
   database: 'nodeskx',
-  password: '',
+  password: 'DragonBall',
   port: 5432,
 })
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  pool.query('SELECT * FROM users u ', (err, resql) => {
-    res.send(resql.rows);
+router.get('/:id', function(req, res, next) {
+  console.log(req.params.id);
+  pool.query('SELECT * FROM users u WHERE id IN ('+req.params.id+')', (err, resql) => {
+    res.json({"data":[resql.rows]});
     console.log(err, resql.rows)
     pool.end()
   })
-  
 });
+
+/* POST user Create */
+router.post('/add/', function(req, res, next){
+  res.json({"message":"ok"})
+});
+
+/* PUT user Modify */
+router.put('/modify/:id', function(req, res, next){
+  res.json({"message":"ok"})
+});
+
+/* DELETE User Delete */
+router.delete('/delete/:id', function(req, res, next){
+  res.json({"message":"ok"})
+});
+
 
 module.exports = router;
